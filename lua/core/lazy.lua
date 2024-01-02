a0186b547621dde8c8c66c08a0e8aa7cc4472241
Moon <101020782+RoniKZX@users.noreply.github.com>
2024-01-02 19:30:26 -0400

    feat(plugin): add indentation blankline

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -74,7 +74,7 @@ require("lazy").setup({
 +#+       +#+ +#+    +#+ +#+    +#+ +#+  +#+#+#
 #+#       #+# #+#    #+# #+#    #+# #+#   #+#+#
 ###       ###  ########   ########  ###    ####
-]]
+      ]]
       logo = string.rep("\n", 8) .. logo .. "\n\n"
 
       local opts = {
@@ -125,7 +125,37 @@ require("lazy").setup({
     end,
     dependencies = {{'nvim-tree/nvim-web-devicons'}}
   },
+
+  -- Indent blankline
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    opts = {
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
+      scope = { enabled = false },
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+      },
+    },
+    main = "ibl",
+  },
 },
+
+-- Lazy options
 {
   performance = {
     rtp = {
