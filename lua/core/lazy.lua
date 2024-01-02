69654ae80eafaba37276889f1283abf0e6703ade
Moon <101020782+RoniKZX@users.noreply.github.com>
2024-01-02 16:59:08 -0400

    feat: add dashboard

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -55,6 +55,46 @@ require("lazy").setup({
       require('neo-tree').setup()
     end
   },
+
+  -- Dashboard
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    config = function()
+      require('dashboard').setup {
+        theme = 'hyper',
+        config = {
+          week_header = {
+            enable = true,
+          },
+          shortcut = {
+            { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+            {
+              icon = ' ',
+              icon_hl = '@variable',
+              desc = 'Files',
+              group = 'Label',
+              action = 'Telescope find_files',
+              key = 'f',
+            },
+            {
+              desc = ' Apps',
+              group = 'DiagnosticHint',
+              action = 'Telescope app',
+              key = 'a',
+            },
+            {
+              desc = ' dotfiles',
+              group = 'Number',
+              action = 'Telescope dotfiles',
+              key = 'd',
+            },
+          },
+        },
+      }
+    end,
+    dependencies = {{'nvim-tree/nvim-web-devicons'}}
+  },
 })
 
 vim.cmd.colorscheme 'rose-pine-moon'
