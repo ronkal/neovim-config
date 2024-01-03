c77ebef303dd738bc5ff8c8ba5a24465900614a7
Moon <101020782+RoniKZX@users.noreply.github.com>
2024-01-03 18:29:22 -0400

    chore(desc): add descriptions

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -373,13 +373,13 @@ require("lazy").setup({
             if vim.wo.diff then return ']c' end
             vim.schedule(function() gs.next_hunk() end)
             return '<Ignore>'
-          end, {expr=true})
+          end, {expr=true, desc = 'Go to next hunk'})
 
           map('n', '[c', function()
             if vim.wo.diff then return '[c' end
             vim.schedule(function() gs.prev_hunk() end)
             return '<Ignore>'
-          end, {expr=true})
+          end, {expr=true, desc = 'Go to prev hunk'})
 
           -- Actions
           map('n', '<leader>hs', gs.stage_hunk, { desc = 'Stage hunk'})
