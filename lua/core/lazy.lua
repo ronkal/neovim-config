8db82748dc7596d983338a7aec972121e718c55e
Moon <101020782+RoniKZX@users.noreply.github.com>
2024-01-02 23:49:56 -0400

    feat(plugins): add which-key

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -275,6 +275,21 @@ require("lazy").setup({
       require('bufferline').setup {}
     end
   },
+
+  -- Which-key
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 500
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 },
 
 -- Lazy options
